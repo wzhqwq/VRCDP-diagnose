ee23053030cb8fbce25d6ff9265b1ea67662f277
wzhqwq <wzhii345@outlook.com>
2026-06-17 19:14:32 +0800

    Enhancement: add dev server

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,4 +9,15 @@ export default defineConfig({
     emptyOutDir: true,
     outDir: '../diagnose/web'
   },
+  server: {
+    port: 5150,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:7653',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '/diagnostics/api'),
+      },
+    },
+  },
 })
